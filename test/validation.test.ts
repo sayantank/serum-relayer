958e822b39aaeeaf0b9088a12819e3f383059b02
Sayantan Karmakar <sayantankarmakar@outlook.com>
2022-07-08 22:32:34 +0530

    new order fee relayer working

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -1,12 +1,18 @@
 import fs from 'fs';
-import { Market } from '@bonfida/dex-v4';
-import { clusterApiUrl, Connection, Keypair, PublicKey } from '@solana/web3.js';
-import { assert } from 'chai';
-import BN from 'bn.js';
+import { initializeAccount, Market, OrderType, Side } from '@bonfida/dex-v4';
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, Transaction } from '@solana/web3.js';
+import axios, { AxiosError } from 'axios';
+import { SelfTradeBehavior } from '@bonfida/dex-v4/dist/state';
+import { Account, getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
+import assert from 'assert';
 
 const MARKET_ADDRESS = new PublicKey('29jz1E8YgfaCS3zmrPttauS4CJS8rZLLL7gdTWFSjWUS');
+
 const BASE_MINT = new PublicKey('3JawYu5tJvG1FiVxtFt27P7Mz4QqoYmzFBvQuJHPnTKs');
 const QUOTE_MINT = new PublicKey('EsJBwWW18Am9uG4G38yE6jtAQqd78Ym5QF8tgHVtCuJj');
+
+const BASE_ACCOUNT = new PublicKey('45uE47VJMvoxcbUZkmdn7xDMomqzkZySxGMBniJDv942');
+const QUOTE_ACCOUNT = new PublicKey('EsgW2983rM3DF82dohCwKPT7CVFJadvnaWxP8mFF7DvQ');
 
 describe('validation', () => {
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
@@ -14,16 +20,94 @@ describe('validation', () => {
     let market: Market;
     let owner: Keypair;
 
+    const alice = Keypair.generate();
+    let aliceQuoteATA: Account;
+    let aliceBaseATA: Account;
+
     before(async () => {
+        const airdropSig = await connection.requestAirdrop(alice.publicKey, 2 * LAMPORTS_PER_SOL);
+        await connection.confirmTransaction(airdropSig, 'confirmed');
+
         const fileBuffer = fs.readFileSync('./keys/octane.json');
         const secretKey: number[] = JSON.parse(fileBuffer.toString());
         owner = Keypair.fromSecretKey(Uint8Array.from(secretKey));
 
         market = await Market.load(connection, MARKET_ADDRESS);
+
+        aliceBaseATA = await getOrCreateAssociatedTokenAccount(
+            connection,
+            alice,
+            BASE_MINT,
+            alice.publicKey,
+            false,
+            'confirmed'
+        );
+        aliceQuoteATA = await getOrCreateAssociatedTokenAccount(
+            connection,
+            alice,
+            QUOTE_MINT,
+            alice.publicKey,
+            false,
+            'confirmed'
+        );
+        await mintTo(connection, alice, BASE_MINT, aliceBaseATA.address, owner, BigInt('100000000000000'));
+        await mintTo(
+            connection,
+            owner,
+            QUOTE_MINT,
+            aliceQuoteATA.address,
+            owner,
+            BigInt('100000000000000'),
+            undefined,
+            {
+                commitment: 'confirmed',
+            }
+        );
+
+        const userIx = await initializeAccount(market.address, alice.publicKey);
+        const tx = new Transaction().add(userIx);
+
+        const sig = await connection.sendTransaction(tx, [alice]);
+        await connection.confirmTransaction(sig, 'confirmed');
     });
 
     it('decode', async () => {
-        console.log(owner.publicKey.toBase58());
-        console.log(market);
+        console.log(`owner: ${owner.publicKey.toBase58()}`);
+        console.log(`alice: ${alice.publicKey.toBase58()}`);
+
+        const ix = await market.makePlaceOrderTransaction(
+            Side.Bid,
+            10,
+            100,
+            OrderType.Limit,
+            SelfTradeBehavior.DecrementTake,
+            aliceQuoteATA.address,
+            alice.publicKey
+        );
+
+        const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash('confirmed');
+        const tx = new Transaction({
+            feePayer: owner.publicKey,
+            blockhash,
+            lastValidBlockHeight,
+        });
+        tx.add(ix);
+        tx.sign(alice);
+
+        const serialized = tx.serialize({
+            requireAllSignatures: false,
+        });
+
+        try {
+            const { data } = await axios.post('http://localhost:3000/api/newOrder', {
+                transaction: serialized.toString('base64'),
+            });
+            console.log(`response: ${data}`);
+        } catch (e) {
+            if (e instanceof AxiosError) {
+                console.error(e.response?.data);
+            }
+            assert(false);
+        }
     });
 });
